4c18bda26c768509b4b4fe6ef315aab172fedc09
Helen Lam <helen.f.lam100@gmail.com>
2014-11-19 14:11:18 -0800

    initial commit

--- app/controllers/cupcakes_controller.rb
+++ app/controllers/cupcakes_controller.rb
@@ -1,21 +1,47 @@
 class CupcakesController < ApplicationController
+
+  attr_reader :sizes, :toppings, :frostings, :ingredients
+
   def vanilla
-    @toppings = ['sprinkles', 'frosting', 'gummy worms']
+    toppings
   end
 
   def chocolate
-    @frostings = {
-      vanilla: '2 dollops',
-      chocolate: 'thin spread',
-      hazelnut: 'the whole jar',
-    }
+    frostings
   end
 
   def maple_bacon
-
+    sizes
   end
 
   def red_velvet
-
+    ingredients
   end
+
+  #--------methods we define to place into the methods above. Creates a cleaner look.------
+
+  def toppings
+    @toppings = ['sprinkles', 'frosting', 'gummy worms']
+  end
+
+  def sizes
+    @sizes = ["small", "medium", "grande"]
+  end
+
+  def frostings
+    @frostings = {
+     vanilla: '2 dollops',
+     chocolate: 'thin spread',
+     hazelnut: 'the whole jar',
+   }
+
+   def ingredients
+     @ingredients = {
+       red_dye: '3 drops',
+       flour: '4 cups',
+       eggs: '3 eggs',
+       baking_powder: '2 spoons',
+     }
+   end
+ end
 end
